28b31d3419462b3e58f524b16b8eff9adf338aa3
denrigtigemarvin <10481788@ucn.dk>
2025-12-09 10:59:15 +0100

    productcontroller

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -1,5 +1,22 @@
 package controller;
 
+import model.Product;
+import model.ProductContainer;
+
 public class ProductController {
+private Product product;
+private ProductContainer productCont;
+private InventoryController inventoryCtrl;
+private InventoryContainer inventoryCont;
+
+public ProductController(Product product, ProductContainer productCont, InventoryController inventoryCtrl,
+		InventoryContainer inventoryCont) {
+	
+	this.product = product;
+	this.productCont = productCont;
+	this.inventoryCtrl = inventoryCtrl;
+	this.inventoryCont = inventoryCont;
+}
+
 
 }
