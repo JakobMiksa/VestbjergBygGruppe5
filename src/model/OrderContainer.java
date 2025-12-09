3c415994ab5b2af1f251cde765b8c06f32f398e3
a <10700486@ucn.dk>
2025-12-09 16:43:38 +0100

    Lavet OrderContainer færdig

--- src/model/OrderContainer.java
+++ src/model/OrderContainer.java
@@ -1,10 +1,51 @@
 package model;
 
+import java.util.ArrayList;
+
 public class OrderContainer {
-
-	public boolean addOrder(Order newOrder) {
-		// TODO Auto-generated method stub
-		return false;
+	private ArrayList<Order> orders;
+	private static OrderContainer instance;
+	
+	private OrderContainer() {
+		orders = new ArrayList<>();
 	}
-
+	
+	public static OrderContainer getInstance() {
+		if (instance == null) {
+			instance = new OrderContainer();
+		} 
+		return instance;
+	}
+	
+	public ArrayList<Order> getOrders() {
+		return orders;
+	}
+	
+	public boolean addOrder(Order newOrder) {
+		boolean res = false;
+		if (orders.size() == 0) {
+			orders.add(newOrder);
+			res = true;
+		} else {
+			for (Order existingOrder : orders) {
+				if (existingOrder.getOrderId() == newOrder.getOrderId()) {
+					System.out.println("Fejl: Order med orderId: " + newOrder.getOrderId() + " findes allerede.);");
+					res = false;
+					return res;
+				}
+			}
+			orders.add(newOrder);
+			res = true;
+		}
+		return res;
+	}
+	
+	public Order findOrder(String orderId) {
+		for (Order order : orders) {
+			if (order.getOrderId() == orderId) {
+				return order;
+			}
+		}
+		return null;
+	}
 }
