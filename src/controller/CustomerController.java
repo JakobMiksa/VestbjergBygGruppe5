d62c2573cf3f68bb58731ff858f2d8fc983eac36
a <10700486@ucn.dk>
2025-12-09 17:39:43 +0100

    CustomerContainer og controller lavet

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -5,24 +5,21 @@ import model.Customer;
 import model.CustomerContainer;
 
 public class CustomerController {
-	private Customer customer;
-	private CustomerContainer customerCont;
-	private OrderController orderController;
+	private CustomerContainer customerCont = CustomerContainer.getInstance();
 	
 	public CustomerController() {
-		this.customerCont = customerCont;
-		this.orderController = orderController;
 	}
 	
 	public Customer createCustomer(String name, String address, String email, String phone, int customerId, double personalDiscount) {
 		Customer customer = new Customer(name, address, email, phone, customerId, personalDiscount);
-		customerCont.addCustomer(customer);
+		boolean success = customerCont.addCustomer(customer);
+		if (!success) {
+			customer = null;
+		}
 		return customer;
-		 
 	}
 	
-	public Customer findCustomer(int CustomerId) {
-		return CustomerCont.getCustomerById(CustomerId);
-		
+	public Customer findCustomer(int customerId) {
+		return customerCont.findCustomer(customerId);
 	}
 }
