a5c9fffd983e87f347faa12638bd7399ee9667ae
nynne19 <10686420@ucn.dk>
2025-12-09 10:46:04 +0100

    Update CustomerController.java
    customercontroller

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,28 @@
+
 package controller;
 
+import model.Customer;
+import model.CustomerContainer;
+
 public class CustomerController {
+	private Customer customer;
+	private CustomerContainer customerCont;
+	private OrderController orderController;
 	
+	public CustomerController() {
+		this.customerCont = new CustomerContainer();
+		this.orderController = orderController;
+	}
+	
+	public Customer createCustomer(int customerId, double personalDiscount) {
+		Customer customer = new Customer(customerId, personalDiscount);
+		customerCont.addCustomer(customer);
+		return customer;
+		 
+	}
+	
+	public Customer findCustomer(int CustomerId) {
+		return CustomerCont.getCustomerById(CustomerId);
+		
+	}
 }
