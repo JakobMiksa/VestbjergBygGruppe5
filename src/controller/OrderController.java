f9a46700aa2ee80b17337aa68b254ffa25ed1317
jakobmiksa <10700486@ucn.dk>
2025-12-09 10:30:28 +0100

    asd

--- src/controller/OrderController.java
+++ src/controller/OrderController.java
@@ -1,5 +1,18 @@
 package controller;
 
+import model.Customer;
+import model.Order;
+import model.Product;
+import model.Staff;
+
 public class OrderController {
-	
+	private Order order;
+	private Product product;
+	private Staff staff;
+	private Customer customer;
+	private CustomerController customerCtrl;
+	private ProductController productCtrl;
+	private InventoryController inventoryCtrl;
+	private StaffController staffCtrl;
+	private LocationController locationCtrl;
 }
