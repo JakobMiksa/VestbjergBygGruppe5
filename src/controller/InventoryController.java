0fd6e3a0abed82c1b692ab06b42de9f50be314cf
nynne19 <10686420@ucn.dk>
2025-12-09 11:34:50 +0100

    Update InventoryController.java

--- src/controller/InventoryController.java
+++ src/controller/InventoryController.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
+import model.InventoryContainer;
+import model.Location;
+
 package controller;
 
 public class InventoryController {
+	 private InventoryContainer inventoryCont;
+	 private Location location;
 
 }
