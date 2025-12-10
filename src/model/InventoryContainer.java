63d3dc5378aa262bd73bd9db0690122c5e6fb340
oscarvk123 <10700464@ucn.dk>
2025-12-10 09:06:27 +0100

    inventoryContainer

---
+++ src/model/InventoryContainer.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InventoryContainer {
+
+    private List<Inventory> inventories;
+
+    public InventoryContainer() {
+        inventories = new ArrayList<>();
+    }
+
+    public void addInventory(Inventory inventory) {
+        inventories.add(inventory);
+    }
+
+    public boolean removeInventory(Inventory inventory) {
+        return inventories.remove(inventory);
+    }
+
+    public Inventory findByProduct(Product product) {
+        for (Inventory inv : inventories) {
+            if (inv.getProduct().equals(product)) {
+                return inv;
+            }
+        }
+        return null;
+    }
+
+    public Inventory findByLocationAndProduct(Location location, Product product) {
+        for (Inventory inv : inventories) {
+            if (inv.getLocation().equals(location) &&
+                inv.getProduct().equals(product)) {
+                return inv;
+            }
+        }
+        return null;
+    }
+
+    public List<Inventory> getAllInventories() {
+        return new ArrayList<>(inventories);
+    }
+
+    public int size() {
+        return inventories.size();
+    }
+}
