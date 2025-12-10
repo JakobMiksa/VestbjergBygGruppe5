7a2e9eef49355ea983272afecc94f8c3339e10b1
oscarvk123 <10700464@ucn.dk>
2025-12-10 13:38:50 +0100

    Date

--- src/model/Order.java
+++ src/model/Order.java
@@ -3,131 +3,139 @@ package model;
 import java.util.ArrayList;
 
 public class Order {
-	private String orderId;
-	private Customer customer;
-	private Staff staff;
-	private ArrayList<OrderLine> orderLine;
-	private OrderStatus orderStatus;
-	private DeliveryStatus deliveryStatus;
-	private double total;
-	private String date;
-	private String expiryDate;
-	
-	public Order() {
-		this.orderLine = new ArrayList<>();
-		this.orderId = "001";
-	}
-	
-	public Order(String orderId, Customer customer, Staff staff, ArrayList<OrderLine> orderLine, OrderStatus orderStatus, DeliveryStatus deliveryStatus, double total, String date, String expiryDate) {
-		this.staff = staff;
-		this.customer = customer;
-		this.orderId = orderId;
-		this.orderStatus = orderStatus;
-		this.deliveryStatus = deliveryStatus;
-		this.total = total;
-		this.date = date;
-		this.expiryDate = expiryDate;
-		this.orderLine = orderLine;
-		
-		orderLine = new ArrayList<>();
-	}
-	
-	public boolean addOrderLine(OrderLine line) {
+    private String orderId;
+    private Customer customer;
+    private Staff staff;
+    private ArrayList<OrderLine> orderLine;
+    private OrderStatus orderStatus;
+    private DeliveryStatus deliveryStatus;
+    private double total;
+    private String date;
+    private String expiryDate;
+
+    public Order() {
+        this.orderLine = new ArrayList<>();
+        this.orderId = generateSKU();
+        this.date = java.time.LocalDate.now().toString();
+    }
+
+    public Order(String orderId, Customer customer, Staff staff, ArrayList<OrderLine> orderLine, OrderStatus orderStatus, DeliveryStatus deliveryStatus, double total, String date, String expiryDate) {
+        this.staff = staff;
+        this.customer = customer;
+        this.orderId = orderId;
+        this.orderStatus = orderStatus;
+        this.deliveryStatus = deliveryStatus;
+        this.total = total;
+        this.date = date;
+        this.expiryDate = expiryDate;
+        this.orderLine = orderLine;
+
+        orderLine = new ArrayList<>();
+    }
+
+    private String generateSKU() {
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            int index = (int) (Math.random() * letters.length());
+            sb.append(letters.charAt(index));
+        }
+
+        sb.append("-");
+
+        for (int i = 0; i < 5; i++) {
+            int digit = (int) (Math.random() * 10);
+            sb.append(digit);
+        }
+
+        return sb.toString();
+    }
+
+    public boolean addOrderLine(OrderLine line) {
         if (line != null) {
             return orderLine.add(line);
         }
         return false;
     }
-	
-	public void recalculateTotal() {
-        double calculatedTotal = 0.0;
 
-        // 1. Sum of all products (existing logic)
+    public void recalculateTotal() {
+        double total = 0.0;
         for (OrderLine line : orderLine) {
-            calculatedTotal += line.getUnitPrice();
+            total += line.getUnitPrice();
         }
-
-        // 2. Delivery Fee Logic
-        double deliveryCost = 500.0; // Set your price here (e.g., 100 kr)
-
-        // Check if the status matches your Enum for delivered
-        if (this.deliveryStatus == DeliveryStatus.delivered) {
-            calculatedTotal += deliveryCost;
-        }
-
-        // 3. Update the final total
-        this.total = calculatedTotal;
+        this.total = total;
     }
-    
-	public Staff getStaff() {
-		return staff;
-	}
 
-	public void setStaff(Staff staff) {
-		this.staff = staff;
-	}
+    public Staff getStaff() {
+        return staff;
+    }
 
-	public Customer getCustomer() {
-		return customer;
-	}
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
 
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
+    public Customer getCustomer() {
+        return customer;
+    }
 
-	public ArrayList<OrderLine> getOrderLine() {
-		return orderLine;
-	}
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 
-	public void setOrderLine(ArrayList<OrderLine> orderLine) {
-		this.orderLine = orderLine;
-	}
+    public ArrayList<OrderLine> getOrderLine() {
+        return orderLine;
+    }
 
-	public String getOrderId() {
-		return orderId;
-	}
+    public void setOrderLine(ArrayList<OrderLine> orderLine) {
+        this.orderLine = orderLine;
+    }
 
-	public void setOrderId(String orderId) {
-		this.orderId = orderId;
-	}
+    public String getOrderId() {
+        return orderId;
+    }
 
-	public OrderStatus getOrderStatus() {
-		return orderStatus;
-	}
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
 
-	public void setOrderStatus(OrderStatus orderStatus) {
-		this.orderStatus = orderStatus;
-	}
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
 
-	public DeliveryStatus getDeliveryStatus() {
-		return deliveryStatus;
-	}
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 
-	public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
-		this.deliveryStatus = deliveryStatus;
-	}
+    public DeliveryStatus getDeliveryStatus() {
+        return deliveryStatus;
+    }
 
-	public double getTotal() {
-		return total;
-	}
+    public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
 
-	public void setTotal(double total) {
-		this.total = total;
-	}
+    public double getTotal() {
+        return total;
+    }
 
-	public String getDate() {
-		return date;
-	}
+    public void setTotal(double total) {
+        this.total = total;
+    }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
+    public String getDate() {
+        return date;
+    }
 
-	public String getExpiryDate() {
-		return expiryDate;
-	}
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-	public void setExpiryDate(String expiryDate) {
-		this.expiryDate = expiryDate;
-	}
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
